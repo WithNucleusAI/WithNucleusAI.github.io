1c14cb59b69479f8b46a8bdd9964ba824419a2c2
Murali Nandan <nmn@withnucleus.ai>
2024-04-30 22:31:16 -0700

    Handled multi-line phrases and Changed content

--- scripts.js
+++ scripts.js
@@ -1,17 +1,32 @@
 const phrases = [
-    "AI reimagined, Products redefined",
-    "From the OG visionaries of AI,",
-    "Perceptive, Creative, Efficient and  Self-Evolving Intelligence.",
+    "The answer, my friend, is blowin' in the wind. <br><br> -Bob Dylan",
+    "Intelligence isn’t compressed memory, it’s the ability to find those answers in the wind.",
+    "The fathers and the prodigies of AI have united, to help AI reach singularity.",
+    //"Perceptive, Creative, Efficient and Self-Evolving Intelligence.",
     "NUCLEUS AI",
     "NUCLEUS. General Intelligence"
 ];
-const prefix = [0,0,0,7,0];
+const prefix = [0,0,0,0,0,0];
+const commaOverrides = {
+    0: {
+        10: 0,
+    },
+};
+const fullstopOverrides = {
+    5: {
+        7: 750,
+    }
+};
+let phrasesWithPause = phrases.map((str, index) => isStringWithPause(str, index));
 
-const typingSpeed = 40;
-const deletingSpeed = 40;
-const delayAfterTyping = 2500;
-const delayAfterTypingWord = 1200;
-const delayAfterDeleting = 1000;
+const typingSpeed = 30;
+var deletingSpeed = 10;
+
+const delayAfterTyping = 3500;
+const delayAfterDeleting = 1500;
+
+const defaultDelayAfterComma = 1500;
+const defaultDelayAfterFullStop = 1500;
 
 let currentPhraseIndex = 0;
 let letterIndex = 0;
@@ -25,25 +40,54 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function isStringWithCommas(str) {
-    const firstIndex = str.indexOf(',');
-    return firstIndex === -1 ? false : firstIndex === str.length - 1 ? false : true;
+function isStringWithPause(str, strIndex) {
+    const firstCommaIndex = str.indexOf(',');
+    const firstFullstopIndex = str.indexOf('.');
+    const isValidComma = firstCommaIndex === -1 ? false : firstCommaIndex === str.length - 1 ? false : true;
+    const isValidFullStop = firstFullstopIndex === -1 ? false : firstFullstopIndex === str.length -1 ? false : true;
+    return isValidComma || isValidFullStop;
 }
 
 async function typeWriter() {
     const currentPhrase = phrases[currentPhraseIndex];
+    if (currentPhraseIndex === phrases.length - 2) deletingSpeed = 65;
     if (isTyping) { 
         if (currentPhraseIndex === phrases.length - 2) textElement.style.fontSize = '3rem';
-        textElement.textContent += currentPhrase.charAt(letterIndex);
+
+        let charToAdd = currentPhrase.charAt(letterIndex);
         letterIndex++;
+
+        // Handling typing of HTML tags
+        if (charToAdd === '<') {
+            while(currentPhrase.charAt(letterIndex) !== '>') {
+                charToAdd += currentPhrase.charAt(letterIndex);
+                letterIndex++;
+            }
+            charToAdd += '>';
+            letterIndex++;
+        }
+
+        textElement.innerHTML += charToAdd;
         
         if (letterIndex < currentPhrase.length) {
-            if (isStringWithCommas(currentPhrase) && currentPhrase.charAt(letterIndex) === ',') await sleep(delayAfterTypingWord);
+            if (phrasesWithPause[currentPhraseIndex]) { 
+                if (currentPhrase.charAt(letterIndex) === ',') {
+                    textElement.innerHTML += ',';
+                    let delay = commaOverrides.hasOwnProperty(currentPhraseIndex) && commaOverrides[currentPhraseIndex].hasOwnProperty(letterIndex) ? commaOverrides[currentPhraseIndex][letterIndex] : defaultDelayAfterComma;
+                    await sleep(delay);
+                    letterIndex++;
+                } else if (currentPhrase.charAt(letterIndex) === '.') {
+                    textElement.innerHTML += '.';
+                    let delay = fullstopOverrides.hasOwnProperty(currentPhraseIndex) && fullstopOverrides[currentPhraseIndex].hasOwnProperty(letterIndex) ? fullstopOverrides[currentPhraseIndex][letterIndex] : defaultDelayAfterFullStop;
+                    await sleep(delay);
+                    letterIndex++;
+                }
+            }
             setTimeout(typeWriter, typingSpeed);
         } else {
             if (currentPhraseIndex === phrases.length - 1) {
                 // cursorElement.style.opacity = '0'; // Hide cursor after typing the last phrase
-                emailElement.style.animationDelay = '3s'; // Start email animation immediately after showing Nucleus AI
+                emailElement.style.animationDelay = '1s'; // Start email animation immediately after showing Nucleus AI
                 emailElement.classList.add('dropDownAnimation');
             } else {
                 isTyping = false;
@@ -52,7 +96,14 @@ async function typeWriter() {
         }
     } else { 
         if (letterIndex > prefix[currentPhraseIndex]) {
-            textElement.textContent = currentPhrase.substring(0, letterIndex - 1);
+            // Handling deletion of HTML tags
+            if (currentPhrase.charAt(letterIndex - 1) === '>') {
+                while(letterIndex > 0 && currentPhrase.charAt(letterIndex - 1) !== '<') {
+                    letterIndex--;
+                }
+                letterIndex--;
+            }
+            textElement.innerHTML = currentPhrase.substring(0, letterIndex - 1);
             letterIndex--;
             setTimeout(typeWriter, deletingSpeed);
         } else {
